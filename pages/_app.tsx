6818474c8da3487d5e08007f825794d6cf392110
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2022-08-05 12:54:37 +0100

    feat: add basic nav (incomplete)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import '../styles/global.scss'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className="wrapper">
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
