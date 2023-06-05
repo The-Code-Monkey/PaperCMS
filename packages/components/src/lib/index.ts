16e317268b80619f9039096df0473e28a5204ada
Andy Wilson <31218527+The-Code-Monkey@users.noreply.github.com>
2023-06-05 15:30:25 +0100

    fix: some changes

--- packages/components/src/lib/index.ts
+++ packages/components/src/lib/index.ts
@@ -1,6 +1,4 @@
 export * from './pages';
 import { SiteThemeProvider } from './components';
-import SupabaseListener from './components/supabase-listener';
-import SupabaseProvider from './components/supabase-provider';
 
-export { SiteThemeProvider, SupabaseProvider, SupabaseListener };
+export { SiteThemeProvider };
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,4 @@
-import useDB from './useDB';
 import { RecordType, CarouselRecordType, ImageRecordType, InnerSectionType, DefaultRecordType } from "./types";
+import useDB from './useDB';
 
 export { useDB, RecordType, ImageRecordType, CarouselRecordType, InnerSectionType, DefaultRecordType };
